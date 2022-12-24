dfd978c9ab79123d88afc5efcc2951f3713dc211
Lucas Costa <lucascosta74@gmail.com>
2022-12-24 11:41:36 -0300

    primeiro commit

---
+++ interface/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>🚧 Em construção 🚧</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
