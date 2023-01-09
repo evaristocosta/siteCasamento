87014a3703ff6f17256c15f080c19d8c3b3cbf2c
Lucas Costa <lucascosta74@gmail.com>
2023-01-09 19:28:52 -0300

    fundo e header do mobile

--- interface/src/pages/Mobile.js
+++ interface/src/pages/Mobile.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Mobile.scss';
 
 function Mobile() {
-  return <div>Mobile</div>;
+  return (
+    <div className="principal">
+      <div className="fundo">
+        <div className="conteudo">
+          <header>
+            <div className="titulo">Ana e Lucas</div>
+            <div className="data">25 de março de 2023</div>
+          </header>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mobile;
