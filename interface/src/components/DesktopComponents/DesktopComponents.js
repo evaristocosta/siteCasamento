ca9eaefd719af1e2d9e015fe3ab709e40a67b26b
Lucas Costa <lucascosta74@gmail.com>
2023-01-13 23:19:22 -0300

    placeholder de desktop

--- interface/src/components/DesktopComponents/DesktopComponents.js
+++ interface/src/components/DesktopComponents/DesktopComponents.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './DesktopComponents.scss';
 import fundoDesktop1 from '../../assets/fundoDesktop1.jpg';
 import logo from '../../assets/logoCasamento.svg';
+import qrcode from '../../assets/qrcode.svg';
 
 function DesktopComponents() {
   return (
@@ -16,6 +17,13 @@ function DesktopComponents() {
             <img src={logo} alt="Logo AL" />
             <div className="data">25 de março de 2023</div>
           </header>
+
+          <div className="centro">
+            <div className="texto-centro">
+              <h1>Acesse esse site pelo seu celular!</h1>
+            </div>
+            <img src={qrcode} alt="QR Code" />
+          </div>
         </div>
       </div>
     </div>
