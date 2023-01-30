46256b6e2b60098a2c41dde08dc418dddc901131
Lucas Costa <lucascosta74@gmail.com>
2023-01-29 21:58:49 -0300

    conexao com bd

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.query('select * from convidados', function (error, results, fields) {
+  if (error) {
+    throw error;
+  }
+
+  console.log(results);
+  console.log(fields);
+});
 
 //const routes = require('./routes');
 const app = express();
